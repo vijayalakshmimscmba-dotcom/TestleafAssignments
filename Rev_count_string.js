4441f01d07a55764b03927e116a372f3dfb140f3
vijayalakshmimscmba-dotcom <vijayalakshmimscmba@gmail.com>
2025-08-23 10:07:37 +0530

    Add files via upload
    Classroom Task 1

---
+++ Rev_count_string.js
@@ -0,0 +1,12 @@
+let stuName="Vijayalakshmi"
+let count = 0
+let rev=""
+for(i=stuName.length-1;i>=0;i--){
+    rev=rev+stuName.charAt(i)
+   count++   
+}
+console.log(rev)
+console.log(count)
+
+
+
